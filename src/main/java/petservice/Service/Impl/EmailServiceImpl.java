cd98ed5cc6d0f8f0f4dca6529f168646b4b73c23
hieudan26 <61266491+hieudan26@users.noreply.github.com>
2022-03-13 18:11:02 +0700

    add host

--- src/main/java/petservice/Service/Impl/EmailServiceImpl.java
+++ src/main/java/petservice/Service/Impl/EmailServiceImpl.java
@@ -90,7 +90,7 @@ public class EmailServiceImpl implements EmailService {
 
     public String templateRegisterMessage(UserEntity user) {
         String token = jwtUtils.generateEmailJwtToken(user.getUserName());
-        String validationLink = host+"confirm-email/="+token;
+        String validationLink = host+"confirm-email/"+token;
         String username = user.getUserName();
         String text="<!DOCTYPE HTML PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional //EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">\n" +
